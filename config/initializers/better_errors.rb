bdec4a0bad8dd902e624c71f85871b07b7b8f1ce
Conner McCabe <conner@koansystems.co.uk>
2014-11-05 14:09:03 +0000

    .gitignore is now working

--- config/initializers/better_errors.rb
+++
@@ -1,6 +0,0 @@
-BetterErrors.editor = :sublime if defined? BetterErrors
-
-if Rails.env == "development"
-	BetterErrors::Middleware.allow_ip! "95.97.80.82/20"
-	BetterErrors::Middleware.allow_ip! "82.71.19.180"
-end
--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BillingSystem::Application.config.secret_token = 'ed1a49b6edb1737f06f382dccd5c04004099a69cd2001bd5ba493edad41eae1a2cb3307cd9cb53a7239744df153cc4f829e29f5d4b44ecbecae7c715ae4a7e40'
